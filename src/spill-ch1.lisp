436d90cd19ba16005f8ce78a943afc358b6c6dca
Brit Butler <brit@kingcons.io>
2018-07-12 17:03:14 -0400

    Add a partial evaluator.

--- src/spill-ch1.lisp
+++ src/spill-ch1.lisp
@@ -1,7 +1,8 @@
 (defpackage :spill-ch1
   (:use :cl :trivia :spill)
   (:import-from :named-readtables #:in-readtable)
-  (:export #:interpret))
+  (:export #:interpret
+           #:pe-compile))
 
 (in-package :spill-ch1)
 
@@ -22,3 +23,27 @@
                (`(+ ,e1 ,e2) (+ (evaluate e1) (evaluate e2))))))
     (match program
       (`(:program ,exp) (evaluate exp)))))
+
+;;; The Partial Evaluator
+
+(defun pe-neg (arg)
+  (if (integerp arg)
+      (- 0 arg)
+      `(- ,arg)))
+
+(defun pe-add (arg1 arg2)
+  (if (and (integerp arg1)
+           (integerp arg2))
+      (+ arg1 arg2)
+      `(+ ,arg1 ,arg2)))
+
+(defun peval (exp)
+  (match exp
+    ((guard exp (integerp exp)) exp)
+    (`(read) '(read))
+    (`(- ,e1) (pe-neg (peval e1)))
+    (`(+ ,e1 ,e2) (pe-add (peval e1) (peval e2)))))
+
+(defun pe-compile (program)
+  (match program
+    (`(:program ,exp) (list :program (peval exp)))))
--- tests/spill.lisp
+++ tests/spill.lisp
@@ -7,8 +7,25 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :spill)' in your Lisp.
 
-(plan 1)
+(plan 4)
 
-(is (spill-ch1:interpret (read-program "tests/test2.lang0")) 42)
+(defun interpret-l0 (form)
+  (spill-ch1:interpret form))
+
+(defun compile-l0 (form)
+  (spill-ch1:interpret (spill-ch1:pe-compile form)))
+
+(defun header (form)
+  (list :program form))
+
+(is (interpret-l0 (read-program "tests/test2.lang0")) 42)
+
+
+(let ((form1 '(+ (read) (- (+ 5 3))))
+      (form2 '(+ 1 (+ (read) 1)))
+      (form3 '(- (+ (read) (- 5)))))
+  (loop for form in (list form1 form2 form3)
+        do (let ((program (header form)))
+             (is (interpret-l0 program) (compile-l0 program)))))
 
 (finalize)
